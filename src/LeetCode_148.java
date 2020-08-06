1acb487cb43dcb915966d85241fc95d043c70439
leviLiang <leviliang@etekcity.com.cn>
2020-08-06 23:15:49 +0800

    add new solutions

--- src/LeetCode_148.java
+++ src/LeetCode_148.java
@@ -28,37 +28,4 @@ public class LeetCode_148 {
         }
         return head;
     }
-
-    public static ListNode sortList_bet(ListNode head) {
-        if (head == null) {
-            return null;
-        }
-        if (head.next == null) {
-            return head;
-        }
-        ListNode init = head.next;
-        ListNode point = head;
-        while (init != null) {
-            if (init.val < point.val) {
-                int i = point.val;
-                point.val = init.val;
-                init.val = i;
-            }
-            ListNode tmp = point;
-            while (tmp != null) {
-                if (tmp.val < init.val && tmp.next.val > init.val) {
-                    init.next = tmp.next;
-                    tmp.next = init;
-                    break;
-                }
-                if (tmp.next == null) {
-                    tmp.next = init;
-                    break;
-                }
-                tmp = tmp.next;
-            }
-            init = init.next;
-        }
-        return head;
-    }
 }
